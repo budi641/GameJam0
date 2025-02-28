6ef0b87b41408fb04ab0cc30f2d84dd13fd2298b
budi641 <aameendev@gmail.com>
2025-02-28 19:05:00 +0200

    Initial commit

---
+++ Source/GameJam0/GameJam0.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameJam0.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameJam0, "GameJam0" );
